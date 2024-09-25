1075d4c5421343da1f8350f7480ca42d1c07c293
mPooja-15 <mishrapooja8128@gmail.com>
2024-09-25 18:57:21 +0530

    fix button responsive

--- src/pages/home/components/HeroSection/HeroSection.tsx
+++ src/pages/home/components/HeroSection/HeroSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './styles.module.css';
 import polygonDown from '../../../../assets/polygon-down.svg'
+import polygonDownMobile from '../../../../assets/poligon-mobile.svg'
 import Image from 'next/image';
 import CommonButton from '@/components/CommonButton/CommonButton';
 
@@ -9,13 +10,14 @@ const HeroSection: React.FC = () => {
   return (
     <section className="flex flex-col gap-4 mt-10 max-sm:mt-0 w-full max-md:mt-10 max-md:max-w-full">
       <div className="flex gap-9 max-w-full w-[449px]">
-        <div className={`flex overflow-hidden flex-col justify-center ${styles.fadeIn}`}>
+        <div className={`flex  flex-col justify-center relative ${styles.fadeIn}`}>
 
-          <CommonButton onClick={() => { }} className="font-black text-[18px] text-[#40E2E8]">
+          <CommonButton onClick={() => {}} className="font-black text-[18px] text-[#40E2E8] max-md:bg-[#8BC4FF] max-md:text-white">
             풀타임, 파트타임
           </CommonButton>
-          <div className="flex gap-2.5 items-start self-start px-2.5">
-            <Image loading="lazy" src={polygonDown} alt="" className="object-contain w-3.5 rounded-sm aspect-square" />
+          <div className="flex gap-2.5 items-start self-start px-2.5 absolute left-0 bottom-[-10px] ">
+            <Image loading="lazy" src={polygonDown} alt="" className="object-contain w-3.5 rounded-sm aspect-square max-md:hidden" />
+            <Image loading="lazy" src={polygonDownMobile} alt="" className="object-contain w-3.5 rounded-sm aspect-square hidden max-md:block " />
           </div>
         </div>
       </div>
