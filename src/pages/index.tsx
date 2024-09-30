94538ab95074df6844a4260cc3f6f605af8d0834
mPooja-15 <mishrapooja8128@gmail.com>
2024-09-30 10:59:30 +0530

    pushed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const HomePage: NextPage<ProfileCardsProps> = ({ users }) => {
 };
 
 export const getServerSideProps = async () => {
-  const res = await fetch('http://localhost:3000/api/users');
+  const res = await fetch(`${process.env.NEXT_BASE_URL}/api/users`);
   const data = await res.json();
 
   return {
