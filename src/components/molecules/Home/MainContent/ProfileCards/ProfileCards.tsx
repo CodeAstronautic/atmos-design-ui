71b01a5b3fc05e687c943c35b3d46e78df50e118
Pooja mishra <mishrapooja8128@gmail.com>
2024-10-09 21:48:49 +0530

    Fixed the build issue

--- src/components/molecules/Home/MainContent/ProfileCards/ProfileCards.tsx
+++ src/components/molecules/Home/MainContent/ProfileCards/ProfileCards.tsx
@@ -73,52 +73,52 @@ const ProfileCards: NextPage<ProfileCardsProps> = ({ users }) => {
   };
 
   return (
-    <div className='w-full max-md:flex justify-center'>
-    <div className={`pt-7 relative slider-cards max-[479px]:pt-6 max-md:pb-6 ${styles.sliderWrapper}`}>
-      <Slider {...settings} ref={sliderRef} initialSlide={1}>
-        {users?.map((person, index) => {
-          const isLeftCard = (activeSlide === 0 && index === users.length - 1) || activeSlide === index + 1;
-          const isRightCard = (activeSlide === users.length - 1 && index === 0) || activeSlide === index - 1;
+    <div className="w-full max-md:flex justify-center">
+      <div className={`pt-7 relative slider-cards max-[479px]:pt-6 max-md:pb-6 ${styles.sliderWrapper}`}>
+        <Slider {...settings} ref={sliderRef} initialSlide={1}>
+          {users?.map((person, index) => {
+            const isLeftCard = (activeSlide === 0 && index === users.length - 1) || activeSlide === index + 1;
+            const isRightCard = (activeSlide === users.length - 1 && index === 0) || activeSlide === index - 1;
 
-          return (
-            <div key={index} className={` ${activeSlide === index ? 'slick-current' : ''}`}>
-              <div
-               className={`bg-white rounded-2xl shadow-md p-9 max-md:px-[16px] text-center ${isLeftCard ? '!bg-[#EDFCFF] max-[1500px]:mx-[23px] max-[1240px]:mx-[115px] max-[1200px]:mx-[105px] max-[768px]:mx-[136px] max-[479px]:mx-[74px] max-[425px]:mx-[60px]' : ''} ${isRightCard ? '!bg-[#EDFCFF] max-[1240px]:mx-[-100px] max-[1200px]:mx-[-80px] max-[768px]:mx-[-100px] max-[479px]:mx-[-47px] max-[425px]:mx-[-22px] max-[375px]:mx-[-20px] max-[320px]:mx-[-54px]' : ''} ${activeSlide === index ? 'w-[291px] max-w-[291px] max-[425px]:w-[234px] max-[320px]:w-[192px]' : 'w-[291px] max-w-[291px] max-[425px]:w-[218px] max-[375px]:w-[204px] max-[360px]:w-[197px]'}`}>
-                <div className="h-[120px] w-[120px] mx-auto rounded-full relative max-md:h-[64px] max-md:w-[69px]">
-                  <Image
-                    loading="lazy"
-                    src={avtar}
-                    alt="user"
-                    className="object-contain max-w-full rounded-none aspect-square w-[120px] h-full"
-                  />
-                  <Image
-                    loading="lazy"
-                    src={flag}
-                    alt="flag"
-                    className="object-contain max-w-full absolute bottom-[-6px] right-4 rounded-none aspect-square max-md:right-0"
-                  />
-                </div>
-                <div className="mt-4">
-                  <h2 className="text-[24px] font-black text-[#24252F] max-md:text-[18px]">{person.name}</h2>
-                  <p className="text-[#4A77FF] text-[16px] font-black max-md:text-[14px]">
-                    {person.role} · {person.experience}
-                  </p>
-                </div>
-                <div className="mt-9 flex flex-wrap gap-1 justify-center">
-                  {person.skills.map((skill, skillIndex) => (
-                    <span
-                      key={skillIndex}
-                      className="inline-block text-[#5E626F] rounded-[6px] py-1 px-3 text-[16px] font-black bg-transparent border border-[#C1C5CF] max-md:text-[14px] leading-[24px]">
-                      {skill}
-                    </span>
-                  ))}
+            return (
+              <div key={index} className={` ${activeSlide === index ? 'slick-current' : ''}`}>
+                <div
+                  className={`bg-white rounded-2xl shadow-md p-9 max-md:px-[16px] text-center ${isLeftCard ? '!bg-[#EDFCFF] max-[1500px]:mx-[23px] max-[1240px]:mx-[115px] max-[1200px]:mx-[105px] max-[768px]:mx-[136px] max-[479px]:mx-[74px] max-[425px]:mx-[60px]' : ''} ${isRightCard ? '!bg-[#EDFCFF] max-[1240px]:mx-[-100px] max-[1200px]:mx-[-80px] max-[768px]:mx-[-100px] max-[479px]:mx-[-47px] max-[425px]:mx-[-22px] max-[375px]:mx-[-20px] max-[320px]:mx-[-54px]' : ''} ${activeSlide === index ? 'w-[291px] max-w-[291px] max-[425px]:w-[234px] max-[320px]:w-[192px]' : 'w-[291px] max-w-[291px] max-[425px]:w-[218px] max-[375px]:w-[204px] max-[360px]:w-[197px]'}`}>
+                  <div className="h-[120px] w-[120px] mx-auto rounded-full relative max-md:h-[64px] max-md:w-[69px]">
+                    <Image
+                      loading="lazy"
+                      src={avtar}
+                      alt="user"
+                      className="object-contain max-w-full rounded-none aspect-square w-[120px] h-full"
+                    />
+                    <Image
+                      loading="lazy"
+                      src={flag}
+                      alt="flag"
+                      className="object-contain max-w-full absolute bottom-[-6px] right-4 rounded-none aspect-square max-md:right-0"
+                    />
+                  </div>
+                  <div className="mt-4">
+                    <h2 className="text-[24px] font-black text-[#24252F] max-md:text-[18px]">{person.name}</h2>
+                    <p className="text-[#4A77FF] text-[16px] font-black max-md:text-[14px]">
+                      {person.role} · {person.experience}
+                    </p>
+                  </div>
+                  <div className="mt-9 flex flex-wrap gap-1 justify-center">
+                    {person.skills.map((skill, skillIndex) => (
+                      <span
+                        key={skillIndex}
+                        className="inline-block text-[#5E626F] rounded-[6px] py-1 px-3 text-[16px] font-black bg-transparent border border-[#C1C5CF] max-md:text-[14px] leading-[24px]">
+                        {skill}
+                      </span>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </Slider>
-    </div>
+            );
+          })}
+        </Slider>
+      </div>
     </div>
   );
 };
